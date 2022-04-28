2698e4ee2aebb073e6a8b58686b3a26beadf1892
EmmanuelM <meric.emmanuel@gmail.com>
2022-04-28 17:18:54 +0200

    react-query base

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {ReactQueryDevtools} from 'react-query/devtools'
+import { QueryClientProvider } from 'react-query'
 import App from './App'
+import {QueryClient} from "react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools/>
+            <App/>
+        </QueryClientProvider>
+    </React.StrictMode>
 )
